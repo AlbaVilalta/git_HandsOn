6d7aa762867d99dace12b2880ef4c3a28c58e51a
Alba Vilalta <alba.vilalta@uvic.cat>
2018-10-09 00:18:35 +0200

    Quart commit

--- seqClass.sh
+++ seqClass.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 seq=$1
 seq=$(echo $seq | tr a-z A-Z)  # Note we just added this line
-if [[ $seq =~ ^[ACGTU]+$ ]]; then
-  if [[ $seq =~ T ]]; then
+if [[ $seq =~ ^[ACGTU]+$ ]]; then 
+  if [[ $seq =~ T ]]; then #si hi ha T es DNA
     echo "The sequence is DNA"
-  elif [[ $seq =~ U ]]; then
+  elif [[ $seq =~ U ]]; then #si hi ha U es RNA
     echo "The sequence is RNA"
   else
-    echo "The sequence can be DNA or RNA"
+    echo "The sequence can be DNA or RNA" #si no hi ha ni T ni U pot ser tan DNA com RNA
   fi
 else
-  echo "The sequence is not DNA nor RNA"
+  echo "The sequence is not DNA nor RNA" #si hi ha algun altre caracter no es ni DNA i RNA
 fi
