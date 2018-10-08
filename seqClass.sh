a7024ac13b9ec6712005c4b0fbed17c48c2c7eff
Alba Vilalta <alba.vilalta@uvic.cat>
2018-10-09 00:43:49 +0200

    Exercise 1

--- seqClass.sh
+++ seqClass.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-seq=$1
+seq=$1 #definim variable
 seq=$(echo $seq | tr a-z A-Z)  # Note we just added this line
-if [[ $seq =~ ^[ACGTU]+$ ]]; then 
-  if [[ $seq =~ T ]]; then #si hi ha T es DNA
-    echo "The sequence is DNA"
-  elif [[ $seq =~ U ]]; then #si hi ha U es RNA
-    echo "The sequence is RNA"
-  else
-    echo "The sequence can be DNA or RNA" #si no hi ha ni T ni U pot ser tan DNA com RNA
-  fi
-else
-  echo "The sequence is not DNA nor RNA" #si hi ha algun altre caracter no es ni DNA i RNA
-fi
+if [[ $seq =~ ^[ACGTU]+$ ]]; then #Si a la sequencia hi ha aquestes lletres: 
+  if [[ $seq =~ T ]]; then #Si hi ha T es DNA
+    echo "The sequence is DNA" #Imprimim que la sequencia es DNA
+  elif [[ $seq =~ U ]]; then #Si hi ha U es RNA
+    echo "The sequence is RNA" #Imprmim que la sequencia es RNA
+  else #Si hi ha qualsevol de les altres:
+    echo "The sequence can be DNA or RNA" #Imprimim que la sequencia pot ser tan DNA com RNA
+  fi #Tanquem un if
+else #Si no hi ha aquestes lletres:
+  echo "The sequence is not DNA nor RNA" #Imprimim que la sequencia no es ni DNA i RNA
+fi #Tanquem l'altre if
